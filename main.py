2b0fcd6bf45654fb5f2b308339b6089ff4c5d67e
Riccardo Zancan <2sleepy@pop-os.localdomain>
2022-12-06 20:14:35 +0100

    ...

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ population_size = 70
 
 
 async def generate_outfit(city, fashion = fashion_default, temperature = 0):
-    city = document.getElementById("citta").value
+    city = Element('citta').element.value
     coords = await get_city_coord(city)
     temperature = await get_today_temperature(coords)
     
